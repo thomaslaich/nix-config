a8e2586692c885a0ed2017d29cea4dae0da25d4c
Thomas Laich <thomaslaich@gmail.com>
2023-11-05 21:03:23 +0100

    some adjustments to doom emacs

--- home/emacs/.doom.d/config.el
+++ home/emacs/.doom.d/config.el
@@ -1,9 +1,3 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; refresh' after modifying this file!
-
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Thomas Laich"
@@ -38,25 +32,25 @@
 (add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
 
 
-;; ;; Here are some additional functions/macros that could help you configure Doom:
-;; ;;
-;; ;; - `load!' for loading external *.el files relative to this one
-;; ;; - `use-package' for configuring packages
-;; ;; - `after!' for running code after a package has loaded
-;; ;; - `add-load-path!' for adding directories to the `load-path', where Emacs
-;; ;;   looks when you load packages with `require' or `use-package'.
-;; ;; - `map!' for binding new keys
-;; ;;
-;; ;; To get information about any of these functions/macros, move the cursor over
-;; ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; ;; This will open documentation for it, including demos of how they are used.
-;; ;;
-;; ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; ;; they are implemented.
+;; Here are some additional functions/macros that could help you configure Doom:
 ;;
-;; ;; Paren pride!
-;; (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
 ;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;; Paren pride!
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
 ;; Fix typing the hash sign in OSX
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
 
@@ -69,9 +63,6 @@
 ;; (setq org-startup-folded 'nil)
 ;;
 ;; (require 'org-drill)
-;;
-;; (add-hook 'enh-ruby-mode-hook 'rvm-activate-corresponding-ruby)
-;; (add-hook 'ruby-mode-hook 'rvm-activate-corresponding-ruby)
 ;;
 ;; Smooth scrolling
 ;; Vertical Scroll
@@ -117,6 +108,8 @@
        "r" #'org-roam-node-find
        "R" #'org-roam-capture))
 
+(after! evil-escape
+  (setq evil-escape-key-sequence "jj"))
 
 ;;; :editor evil
 ;; Focus new window after splitting
