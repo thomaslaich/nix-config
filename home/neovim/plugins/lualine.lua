22046e59e7cce65b4c68971bb84abe52cb9bc903
Thomas Laich <thomaslaich@gmail.com>
2023-11-11 16:02:58 +0100

    Fix lualine

--- home/neovim/plugins/lualine.lua
+++ home/neovim/plugins/lualine.lua
@@ -2,9 +2,6 @@ if not vim.g.vscode then
   require("lualine").setup({
     options = {
       theme = "tokyonight",
-      section_separators = { "", "" },
-      component_separators = { "", "" },
-      icons_enabled = true,
     },
     sections = {
       lualine_a = { { "mode", upper = true } },
@@ -16,14 +13,6 @@ if not vim.g.vscode then
       lualine_x = { "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
     },
     extensions = { "fzf" },
   })
