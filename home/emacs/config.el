41faa8c76fbcafab533b7b88c2f85bf77d261526
Thomas Laich <thomaslaich@gmail.com>
2023-11-13 17:30:38 +0100

    Update Emacs config: Add company, magit, and projectile

--- home/emacs/config.el
+++ home/emacs/config.el
@@ -1,5 +1,6 @@
 ;;; use-package is added automatically by the nix emacs overlay
 ;;; here we just make sure we don't have to keep typing `:ensure t`
+
 (setq use-package-always-ensure t)
 
 ;;; GENERAL UI
@@ -19,7 +20,6 @@
 (global-visual-line-mode t)
 (setq display-line-numbers-type 'relative)
 
-
 ;;; FONTS AND THEMES
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
 (set-face-attribute 'variable-pitch nil :font "Ubuntu Nerd Font" :height 140)
@@ -37,30 +37,14 @@
 (setq catppuccin-flavor 'macchiato) ;; or 'latte, 'macchiato, or 'mocha
 (catppuccin-reload)
 
-;; (use-package doom-themes
-;;   :config
-;;   ;; Global settings (defaults)
-;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;;         doom-themes-enable-italic t
-;;         doom-themes-padded-modeline t) ; if nil, italics is universally disabled
-;;   ;; (load-theme 'doom-one t)
-;;   (load-theme 'doom-tokyo-night t))
-;; 
-;;   ;; Enable flashing mode-line on errors
-;;   ;; (doom-themes-visual-bell-config)
-;;   ;; Enable custom neotree theme (all-the-icons must be installed!)
-;;   ;; (doom-themes-neotree-config)
-;;   ;; or for treemacs users
-;;   ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-;;   ;; (doom-themes-treemacs-config)
-;;   ;; Corrects (and improves) org-mode's native fontification.
-;;   ;; (doom-themes-org-config))
-
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
 
-;; (use-package all-the-icons
-;;   :if (display-graphic-p))
+;; tranparency (doing nothing on MacOS :( )
+(add-to-list 'default-frame-alist '(alpha-background . 90))
+
+;; Hide minor modes from modeline (by adding :diminish to use-package)
+(use-package diminish)
 
 ;;; EVIL MODE
 
@@ -80,7 +64,7 @@
 (use-package evil-collection
   :after evil
   :config
-  (setq evil-collection-mode-list '(dashboard dired ibuffer))
+  (setq evil-collection-mode-list '(dashboard dired ibuffer magit))
   (evil-collection-init))
 
 (use-package evil-tutor)
@@ -95,6 +79,17 @@
   (setq-default evil-escape-delay 0.2)
   (setq-default evil-escape-key-sequence "jj")
   (evil-escape-mode))
+
+;; commentary
+(use-package evil-commentary
+  :after evil
+  :config
+  (evil-commentary-mode))
+
+;; surround
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1))
 
 ;;; KEYBINDINGS
 ;; (use-package bind-key
@@ -121,12 +116,6 @@
   :prefix "," ; set leader
   :global-prefix "M-,") ; access leader in insert mode (do we need this?)
 
-(general-create-definer window-def
-  :states '(normal visual insert emacs)
-  :keymaps 'override
-  :prefix "C-w")
-
-
 ;; zoom keybindings
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
@@ -142,68 +131,69 @@
 ;; buffer keybindings
 (leader-def
   "b" '(:ignore t :wk "[B]uffer")
-  "b b" '(switch-to-buffer :wk "Switch [B]uffer")
-  "b i" '(ibuffer :wk "[I]buffer")
-  "b k" '(kill-current-buffer :wk "[K]ill Buffer")
-  "b n" '(next-buffer :wk "[N]ext Buffer")
-  "b p" '(previous-buffer :wk "[P]revious Buffer")
-  "b r" '(revert-buffer :wk "[R]evert Buffer"))
+  "b b" '(switch-to-buffer :wk "Switch Buffer")
+  "b i" '(ibuffer :wk "Ibuffer")
+  "b k" '(kill-current-buffer :wk "Kill Buffer")
+  "b n" '(next-buffer :wk "Next Buffer")
+  "b p" '(previous-buffer :wk "Previous Buffer")
+  "b r" '(revert-buffer :wk "Revert Buffer"))
 
 ;; evaluate elisp keybindings
 (leader-def
   "e" '(:ignore t :wk "[E]valuate")
-  "e b" '(eval-buffer :wk "Evaluate elisp in [b]uffer")
-  "e d" '(eval-defun :wk "Evaluate elisp in [d]efun")
-  "e e" '(eval-expression :wk "Evaluate elisp [e]xpression")
-  "e l" '(eval-last-sexp :wk "Evaluate elisp in [l]ast sexp")
-  "e r" '(eval-region :wk "Evaluate elisp in [r]egion"))
+  "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
+  "e d" '(eval-defun :wk "Evaluate elisp in defun")
+  "e e" '(eval-expression :wk "Evaluate elisp expression")
+  "e l" '(eval-last-sexp :wk "Evaluate elisp in last sexp")
+  "e r" '(eval-region :wk "Evaluate elisp in region"))
 
 ;; help keybindings
 (leader-def
   "h" '(:ignore t :wk "[H]elp")
-  "h a" '(apropos :wk "[A]propos")
-  "h c" '(describe-char :wk "[C]haracter")
-  "h f" '(describe-function :wk "[F]unction")
-  "h k" '(describe-key :wk "[K]ey")
-  "h m" '(describe-mode :wk "[M]ode")
-  "h p" '(describe-package :wk "[P]ackage")
-  "h v" '(describe-variable :wk "[V]ariable"))
+  "h a" '(apropos :wk "Apropos")
+  "h c" '(describe-char :wk "Character")
+  "h f" '(describe-function :wk "Function")
+  "h k" '(describe-key :wk "Key")
+  "h m" '(describe-mode :wk "Mode")
+  "h p" '(describe-package :wk "Package")
+  "h v" '(describe-variable :wk "Variable"))
   ;; need to add "h r r" for reloading config as well?
 
 ;; search
 (leader-def
   "s" '(:ignore t :wk "[S]earch")
-  "s f" '(consult-find :wk "[S]earch [F]iles")
-  "s b" '(consult-buffer :wk "[S]earch [B]uffer")
+  "s f" '(consult-find :wk "Search Files")
+  "s b" '(consult-buffer :wk "Search Buffer")
+  "s /" '(consult-buffer :wk "Search Buffer")
   ;; TODO maybe add "s a" as in neovim?
-  "s g" '(consult-ripgrep :wk "[S]earch by [G]rep")
-  "s h" '(consult-man :wk "[S]earch [H]elp")
-  "s i" '(info :wk "[S]earch [I]nfo")
-  "s r" '(consult-recent-file :wk "[S]earch [R]ecent Files"))
+  "s g" '(consult-ripgrep :wk "Search by [G]rep")
+  "s h" '(consult-man :wk "Search Help")
+  "s i" '(info :wk "Search Info")
+  "s r" '(consult-recent-file :wk "Search Recent Files"))
 
 ;; Git
 (leader-def
   "g" '(:ignore t :wk "[G]it")
-  "g f" '(consult-git-grep :wk "[F]ind in Git")
+  "g f" '(consult-git-grep :wk "Find in Git")
   ;; TODO maybe at "g c" and "g b" as in neovim?
-  "g s" '(magit-status :wk "Git [S]tatus"))
+  "g g" '(magit-status :wk "Magit"))
 
 
 ;; toggle keybindings
 (leader-def
   "t" '(:ignore t :wk "[T]oggle")
-  "t l" '(display-line-numbers-mode :wk "[T]oggle [L]ine Numbers")
-  "t t" '(global-visual-line-mode :wk "[T]oggle [T]runcate Lines")
-  "t v" '(vterm-toggle :wk "[T]oggle [V]term"))
+  "t l" '(display-line-numbers-mode :wk "Toggle Line Numbers")
+  "t t" '(global-visual-line-mode :wk "Toggle Truncate Lines")
+  "t v" '(vterm-toggle :wk "Toggle Vterm"))
 
 ;; org keybindings
 (leader-def
   "o" '(:ignore t :wk "[O]rg")
-  "o a" '(org-agenda :wk "[A]genda")
-  "o c" '(org-capture :wk "[C]apture")
-  "o l" '(org-store-link :wk "[L]ink")
-  "o r" '(org-refile :wk "[R]efile")
-  "o t" '(org-todo :wk "[T]odo"))
+  "o a" '(org-agenda :wk "Agenda")
+  "o c" '(org-capture :wk "Capture")
+  "o l" '(org-store-link :wk "Link")
+  "o r" '(org-refile :wk "Refile")
+  "o t" '(org-todo :wk "Todo"))
 
 ;; Window motions vim mode
 (global-set-key (kbd "C-h") 'evil-window-left)
@@ -211,36 +201,25 @@
 (global-set-key (kbd "C-k") 'evil-window-up)
 (global-set-key (kbd "C-l") 'evil-window-right)
 
-(window-def
-  "h" '(evil-window-left :wk "Move Horizontally Left")
-  "j" '(evil-window-down :wk "Move Horizontally Down")
-  "k" '(evil-window-up :wk "Move Horizontally Up")
-  "l" '(evil-window-right :wk "Move Horizontally Right")
-
-  "c" '(evil-window-delete :wk "[C]lose Current Window")
-  "n" '(evil-window-new :wk "[N]ew Window")
-  "s" '(evil-window-split :wk "[S]plit (Horizontally)")
-  "v" '(evil-window-vsplit :wk "Split [V]ertically"))
-
 (leader-def
   "w" '(:ignore t :wk "[W]indows")
 
   ;; Window splits
-  "w c" '(evil-window-delete :wk "[C]lose Current Window")
-  "w n" '(evil-window-new :wk "[N]ew Window")
-  "w s" '(evil-window-split :wk "[S]plit (Horizontally)")
-  "w v" '(evil-window-vsplit :wk "Split [V]ertically")
-  "w o" '(delete-other-windows :wk "[O]nly Window")
-  "w =" '(balance-windows :wk "Balance [W]indows")
-  "w |" '(evil-window-set-width :wk "Set Window [W]idth")
-  "w _" '(evil-window-set-height :wk "Set Window [H]eight")
+  "w c" '(evil-window-delete :wk "Close Current Window")
+  "w n" '(evil-window-new :wk "New Window")
+  "w s" '(evil-window-split :wk "Split (Horizontally)")
+  "w v" '(evil-window-vsplit :wk "Split Vertically")
+  "w o" '(delete-other-windows :wk "Close Other Windows")
+  "w =" '(balance-windows :wk "Balance Windows")
+  "w |" '(evil-window-set-width :wk "Set Window Width")
+  "w _" '(evil-window-set-height :wk "Set Window Height")
 
   ;; Window motions
-  "w h" '(evil-window-left :wk "Move Horizontally Left")
-  "w j" '(evil-window-down :wk "Move Horizontally Down")
-  "w k" '(evil-window-up :wk "Move Horizontally Up")
-  "w l" '(evil-window-right :wk "Move Horizontally Right")
-  "w w" '(evil-window-next :wk "Next [W]indow")
+  "w h" '(evil-window-left :wk "Move Left")
+  "w j" '(evil-window-down :wk "Move Down")
+  "w k" '(evil-window-up :wk "Move Up")
+  "w l" '(evil-window-right :wk "Move Right")
+  "w w" '(evil-window-next :wk "Next Window")
 
   ;; Move windows
   "w H" '(buf-move-left :wk "Buffer Move Left")
@@ -248,41 +227,52 @@
   "w K" '(buf-move-up :wk "Buffer Move Up")
   "w L" '(buf-move-right :wk "Buffer Move Right"))
 
-  ;; Code keybindings
-  (leader-def
-    "c" '(:ignore t :wk "[C]ode")
-    "c c" '(compile :wk "[C]ompile")
-    "c d" '(lsp-find-definition :wk "[D]efinition")
-    "c f" '(lsp-format-buffer :wk "[F]ormat Buffer")
-    "c i" '(lsp-organize-imports :wk "[I]mports")
-    "c r" '(lsp-find-references :wk "[R]eferences")
-    "c t" '(lsp-find-type-definition :wk "[T]ype Definition"))
+;; Code keybindings
+(leader-def
+"c" '(:ignore t :wk "[C]ode")
+"c c" '(compile :wk "Compile"))
 
-  ;; LSP keybindings
-  ;; prefixed under leader
-  (leader-def
-    "l" '(:ignore t :wk "[L]SP")
-    "l d" '(lsp-find-definition :wk "[D]efinition")
-    "l r" '(lsp-find-references :wk "[R]eferences")
-    "l i" '(lsp-organize-imports :wk "[I]mports")
-    "l f" '(lsp-format-buffer :wk "[F]ormat Buffer"))
+;; LSP only keybindings
+(add-hook 'lsp-mode-hook
+          (lambda ()
+            (progn 
+              (leader-def
+                "l" '(:ignore t :wk "[L]SP")
+                "l d" '(lsp-find-definition :wk "[D]efinition")
+                "l r" '(lsp-find-references :wk "[R]eferences")
+                "l i" '(lsp-organize-imports :wk "[I]mports")
+                "l f" '(lsp-format-buffer :wk "[F]ormat Buffer")
 
-  ;; local leader shorts
-  (local-leader-def
-    "d" `(lsp-find-definition :wk "[D]efinition")
-    "f" `(lsp-format-buffer :wk "[F]ormat Buffer"))
+                "c d" '(lsp-find-definition :wk "Definition")
+                "c f" '(lsp-format-buffer :wk "Format Buffer")
+                "c i" '(lsp-organize-imports :wk "Imports")
+                "c r" '(lsp-find-references :wk "References")
+                "c t" '(lsp-find-type-definition :wk "Type Definition"))
 
-  ;; TODO not working
-  ;; standardized
-  ;; (global-set-key (kbd "g d") '(lsp-find-definition :wk "[D]efinition"))
-  ;; (global-set-key "g D" '(lsp-find-declaration :wk "[D]eclaration"))
-  ;; (global-set-key "g i" '(lsp-find-implementation :wk "[I]mplementation"))
-  ;; (global-set-key "g r" '(lsp-find-references :wk "[R]eferences"))
-  ;; (global-set-key (kbd "K") '(lsp-describe-thing-at-point :wk "[K]ind"))
+              (local-leader-def
+                "d" `(lsp-find-definition :wk "Definition")
+                "f" `(lsp-format-buffer :wk "Format Buffer"))
+            ))
+          )
+
+;; local leader shorts
+
+(leader-def "p" '(projectile-command-map :wk "[P]rojects"))
+
+;; TODO not working
+;; standardized
+(define-key evil-motion-state-map (kbd "gd") 'lsp-find-declaration)
+
+;; (global-set-key (kbd "g d") '(lsp-find-definition :wk "[D]efinition"))
+;; (global-set-key "g D" '(lsp-find-declaration :wk "[D]eclaration"))
+;; (global-set-key "g i" '(lsp-find-implementation :wk "[I]mplementation"))
+;; (global-set-key "g r" '(lsp-find-references :wk "[R]eferences"))
+;; (global-set-key (kbd "K") '(lsp-describe-thing-at-point :wk "[K]ind"))
 
 ;;; WHICH-KEY
 (use-package which-key
   :init (which-key-mode 1)
+  :diminish
   :config
   (setq which-key-side-window-location 'bottom
         which-key-sort-order #'which-key-key-order-alpha
@@ -314,61 +304,6 @@
 
 ;;; COMPLETION (VERTICO&CONSULT)
 ;; NOTE: use Vertico&Consult instead of Ivy&Counsel
-
-(use-package vertico
-  :init
-  (vertico-mode)
-  ;; TODO maybe we want to add one of these?
-  
-  ;; Different scroll margin
-  ;; (setq vertico-scroll-margin 0)
-
-  ;; Show more candidates
-  ;; (setq vertico-count 20)
-
-  ;; Grow and shrink the Vertico minibuffer
-  ;; (setq vertico-resize t)
-
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  ;; (setq vertico-cycle t)
-  )
-
-;; Persist history over Emacs restarts. Vertico sorts by history position.
-(use-package savehist
-  :init
-  (savehist-mode))
-
-;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-                  (replace-regexp-in-string
-                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                   crm-separator)
-                  (car args))
-          (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
-
-;; Optionally use the `orderless' completion style.
-(use-package orderless
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
 
 ;; Grepping using Consult
 ;; Example configuration for Consult
@@ -429,6 +364,61 @@
   ;; package.
   (marginalia-mode))
 
+(use-package vertico
+  :init
+  (vertico-mode)
+  ;; TODO maybe we want to add one of these?
+  
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
 ;;; TERMINAL EMULATION
 
 (use-package vterm
@@ -439,11 +429,12 @@
 
 (use-package vterm-toggle
   :after vterm
-  :config
-  (setq vterm-toggle-fullscreen-p nil
-        vterm-toggle-scope 'project
-        vterm-toggle-cd-auto-create-buffer nil
-        vterm-toggle-cd-auto-run-dired nil))
+  ;; ;; :config
+  ;; (setq vterm-toggle-fullscreen-p nil
+  ;;       vterm-toggle-scope 'project
+  ;;       vterm-toggle-cd-auto-create-buffer nil
+  ;;       vterm-toggle-cd-auto-run-dired nil)
+  )
 
 ;;; COPILOT
 
@@ -453,7 +444,7 @@
 
 ;;; LANGUAGES
 
-;;; lsp-mode
+;;; LSP-MODE
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :init
@@ -464,6 +455,12 @@
   :hook
   ;; C#
   (csharp-mode . lsp-deferred))
+
+;; completion
+(use-package company)
+(add-hook 'after-init-hook 'global-company-mode)
+(use-package company-box
+  :hook (company-mode . company-box-mode))
 
 
 ;; Lispy (for emacs lisp)
@@ -516,3 +513,27 @@
   (use-package which-key
       :config
       (which-key-mode))
+
+;;; PROJECTS AND GIT INTEGRATION
+
+;; Projectile
+(use-package projectile
+  :init
+  (projectile-mode +1)
+  (setq projectile-project-search-path '("~/repos/"))
+  (setq projectile-switch-project-action #'projectile-dired)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
+;; Projectile consult integration
+(use-package consult-projectile)
+
+;; Magit
+(use-package magit
+  :commands magit-status
+  :bind (("C-x g" . magit-status)))
+
+;; now part of evil-collection?
+;; (use-package evil-magit
+;;   :after magit)
