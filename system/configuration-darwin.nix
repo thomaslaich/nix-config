ac76aedf4daf2583b711548b97aa95121c9c5d9b
Thomas Laich <thomaslaich@gmail.com>
2024-01-19 21:59:25 +0100

    add github workflow, fix flake errors

--- system/configuration-darwin.nix
+++ system/configuration-darwin.nix
@@ -12,7 +12,13 @@
 
   nixpkgs = {
     # You can add overlays here
-    overlays = outputs.overlays;
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      # outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.stable-packages
+    ];
+    # Configure your nixpkgs instance
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
