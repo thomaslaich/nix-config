dff032c168de7bbf1581a7d48ac693edd0927539
Thomas Laich <thomaslaich@gmail.com>
2024-05-23 22:43:35 +0200

    further emacs improvements (folding, default formatters, lsp keybindings)

--- home/neovim/plugins/lspconfig.lua
+++ home/neovim/plugins/lspconfig.lua
@@ -26,7 +26,10 @@ lsp_config.tsserver.setup({})
 -- outside the module. Until then, let's use omnisharp-roslyn for .NET
 lsp_config.omnisharp.setup({
   handlers = {
-    ["textDocument/definition"] = require("omnisharp_extended").handler,
+    ["textDocument/definition"] = require("omnisharp_extended").definition_handler,
+    ["textDocument/typeDefinition"] = require("omnisharp_extended").type_definition_handler,
+    ["textDocument/references"] = require("omnisharp_extended").references_handler,
+    ["textDocument/implementation"] = require("omnisharp_extended").implementation_handler,
   },
   cmd = { omnisharpBin },
 })
