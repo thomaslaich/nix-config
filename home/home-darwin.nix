731467428f3b7552bdef44d304b01fd78136c952
Thomas Laich <thomaslaich@gmail.com>
2024-10-06 16:57:07 +0200

    update system after macos sequoia update

--- home/home-darwin.nix
+++ home/home-darwin.nix
@@ -11,15 +11,15 @@
     mos # smooth scrolling
   ];
 
-  home.activation = {
-    rsync-home-manager-applications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
-      apps_source="$genProfilePath/home-path/Applications"
-      moniker="Home Manager Trampolines"
-      app_target_base="${config.home.homeDirectory}/Applications"
-      app_target="$app_target_base/$moniker"
-      mkdir -p "$app_target"
-      ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
-    '';
-  };
+  # home.activation = {
+  #   rsync-home-manager-applications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  #     rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
+  #     apps_source="$genProfilePath/home-path/Applications"
+  #     moniker="Home Manager Trampolines"
+  #     app_target_base="${config.home.homeDirectory}/Applications"
+  #     app_target="$app_target_base/$moniker"
+  #     mkdir -p "$app_target"
+  #     ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
+  #   '';
+  # };
 }
