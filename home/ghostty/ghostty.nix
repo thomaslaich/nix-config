edd1dc1beb4f74e11d72d34f8f3d70edaa13c748
Thomas Laich <thomaslaich@gmail.com>
2025-02-23 22:33:22 +0100

    increase ghostty font-size

--- home/ghostty/ghostty.nix
+++ home/ghostty/ghostty.nix
@@ -5,6 +5,7 @@
     enable = true;
     settings = {
       macos-option-as-alt = true;
+      font-size = 14;
     };
     package = if pkgs.stdenv.hostPlatform.isDarwin then null else pkgs.ghostty; # currently marked broken on Darwin
   };
