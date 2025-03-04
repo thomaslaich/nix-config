6fbdda1c187bd7bae20da267d59a06aecec64c90
Thomas Laich <thomaslaich@gmail.com>
2025-03-04 14:22:10 +0100

    update config for darwin after update

--- system/configuration-darwin.nix
+++ system/configuration-darwin.nix
@@ -32,10 +32,10 @@
     };
   };
 
-  services.emacs.enable = true;
+  # services.emacs.enable = true;
 
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   system = {
     keyboard.enableKeyMapping = true;
     keyboard.remapCapsLockToControl = true;
