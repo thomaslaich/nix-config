22cea30c409cd553b8e3e6d4d8b4674e08ae4cb0
Thomas Laich <thomaslaich@gmail.com>
2025-03-04 22:14:57 +0100

    disable notmuch temporarily

--- home/email/email.nix
+++ home/email/email.nix
@@ -6,12 +6,12 @@
     extraConfig = builtins.readFile ./.mbsyncrc;
   };
   programs.msmtp.enable = true;
-  programs.notmuch = {
-    enable = true;
-    hooks = {
-      preNew = "mbsync -a";
-    };
-  };
+  # programs.notmuch = {
+  #   enable = true;
+  #   hooks = {
+  #     preNew = "mbsync -a";
+  #   };
+  # };
   accounts.email = {
     accounts.gmail = {
       address = "thomaslaich@gmail.com";
@@ -21,7 +21,7 @@
       #   create = "maildir";
       # };
       msmtp.enable = true;
-      notmuch.enable = true;
+      # notmuch.enable = true;
       primary = true;
       realName = "Thomas Laich";
       smtp = {
