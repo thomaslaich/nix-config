d7d60c285ac9695bc03a4ccb8c763f581752d3d8
Thomas Laich <thomaslaich@gmail.com>
2025-06-07 14:55:24 +0200

    update nixos config

--- system/configuration-nixos.nix
+++ system/configuration-nixos.nix
@@ -56,9 +56,9 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
+  services.desktopManager.gnome.enable = true;
 
   # Disable a few gnome packages
   environment.gnome.excludePackages = with pkgs; [
@@ -89,7 +89,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
